26f3ea4950b7dcbbf21bff9ec4acc64ad9462ea4
Sofiia <voznyakson@gmail.com>
2023-01-07 17:05:41 +0200

    connection to sqlite db

---
+++ pyflask3/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='VERY_SECRET_KEY',
+        DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    #for new functionality
+
+    return app
---
+++ pyflask3/schema.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS currencies;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS records;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE currencies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name INTEGER NOT NULL
+);
+
+CREATE TABLE categories (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name INTEGER NOT NULL
+);
+
+CREATE TABLE records (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  currency_id INTEGER NOT NULL,
+  category_id INTEGER NOT NULL,
+  date DATETIME NOT NULL,
+  cost INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (currency_id) REFERENCES currencies (id),
+  FOREIGN KEY (category_id) REFERENCES categories (id)
+);
